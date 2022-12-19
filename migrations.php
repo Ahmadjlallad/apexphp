16ee14076adbcf0b9fe1c72163e3efb6f3eb7e77
ahmad jallad <ahmad.jallad@altibbi.com>
2022-12-19 17:21:40 +0300

    migration

---
+++ migrations.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Apex\src\App;
+
+$dotenv = Dotenv\Dotenv::createImmutable((__DIR__));
+$dotenv->load();
+$config = [
+    'db' => [
+        'type' => $_ENV['DB_TYPE'],
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'name' => $_ENV['DB_NAME'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ],
+    'ROOT_DIR' => __DIR__
+];
+
+
+$app = new App($config);
+$app->db->appleyMigration();
--- src/App.php
+++ src/App.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Apex\src;
 
+use Apex\src\Database\Database;
 use Apex\src\Router\Router;
 use Apex\src\View\View;
 use JetBrains\PhpStorm\NoReturn;
@@ -11,24 +12,36 @@ class App
 {
     public static string $ROOT_DIR;
     public static string $VIEWS_DIR;
+    static public array $config = [];
+    private static App $instance;
     public Router $router;
     public Request $request;
     public Response $response;
     public View $view;
-    private static App $instance;
+    public Database $db;
 
-    #[NoReturn] public function __construct($ROOT_DIR = null, $VIEWS_DIR = null)
+    #[NoReturn] public function __construct($config = [])
     {
+        self::$ROOT_DIR = $config['ROOT_DIR'] ?? dirname(__DIR__);
+        self::$VIEWS_DIR = $config['VIEWS_DIR'] ?? self::$ROOT_DIR . '/resources/views';
         static::$instance = $this;
-        self::$ROOT_DIR = $ROOT_DIR ?? dirname(__DIR__);
-        self::$VIEWS_DIR = $VIEWS_DIR ?? self::$ROOT_DIR . '/resources/views';
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router();
         $this->view = new View();
+        $this->db = new Database($config['db'] ?? static::$config['db']);
+        static::$config = $config;
     }
 
-    /** TODO */
+    public static function getInstance(): App
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
     public function run(): void
     {
         $res = $this->router->resolve();
@@ -37,14 +50,5 @@ class App
             dd('VIEW NOT FOUND you maybe forget to return form controller');
         }
         echo $res;
-    }
-
-    public static function getInstance(): App
-    {
-        if (is_null(static::$instance)) {
-            static::$instance = new static;
-        }
-
-        return static::$instance;
     }
 }
