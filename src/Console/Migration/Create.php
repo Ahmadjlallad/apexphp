cd3aa80ccb9a75dff0efee7e453aac753eed222b
Ahmad Jallad <ahmadjlad@gmail.com>
2023-02-14 20:30:45 +0300

    create to console commands for migration instead of using migration file

---
+++ src/Console/Migration/Create.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Apex\src\Console\Migration;
+
+use Carbon\Carbon;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Create extends Migration
+{
+    protected function configure()
+    {
+        $this
+            ->addArgument('name', InputArgument::REQUIRED, 'migration file name');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $migrationName = $input->getArgument('name');
+        if (preg_match('/[\'^£$%&*()}{@#~?><,|=+¬-]/', $migrationName)) {
+            $output->writeln('<error>Migration file name shouldn\'t contain spacial characters</error>');
+            return Command::FAILURE;
+        }
+        $migrationName = 'm' . Carbon::now()->unix() . '_' . $migrationName;
+        $migrationTemplate = str_replace('@className', $migrationName, file_get_contents(dirname(__DIR__) . '/Migration/migration.example'));
+        if (!file_put_contents($_SERVER['PWD'] . '/migrations/' . $migrationName . '.php', $migrationTemplate)) {
+            $output->writeln('<error>Couldn\'t create a migration file</error>');
+            return Command::FAILURE;
+        }
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Console/Migration/Up.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Apex\src\Console\Migration;
+
+use Apex\src\Database\Migration\ExecuteMigrations;
+use Apex\src\Database\Migration\Schema\SchemaBuilder;
+use Dotenv\Dotenv;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Apex\src\App;
+class Up extends Migration
+{
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        (new ExecuteMigrations(App::getInstance()->db->pdo, new SchemaBuilder()))->applyMigration();
+        return Command::SUCCESS;
+    }
+}
