d09244094d5cefe7d9542e915fae9c7c460ad427
Ahmad Jallad <ahmadjlad@gmail.com>
2023-02-18 14:49:54 +0300

    add array trite helper make dir for helpers

---
+++ src/Helpers/Arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Apex\src\Helpers;
+
+trait Arrays
+{
+    function isAssoc(array $arr): bool
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+}
